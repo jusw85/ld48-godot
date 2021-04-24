f7547b36757ac7665deecfed9297b5b5143a3f0f
Justin Wong <jusw85@hotmail.com>
2021-04-24 17:44:34 +0800

    Convert tiles to objects

--- main.gd
+++ main.gd
@@ -1,13 +1,36 @@
 extends Node2D
 
 
+const Fuel := preload("res://collectibles/fuel.tscn")
+const Gem := preload("res://collectibles/gem.tscn")
 onready var tilemap: TileMap = $"Level/TileMap"
 
-var bounds_min: Vector2
-var bounds_max: Vector2
+var bounds_min := Vector2(INF, INF)
+var bounds_max := Vector2(-INF, -INF)
 
 func _ready() -> void:
 	_calculate_map_bounds()
+	_relace_map_tiles_with_objects()
+
+
+func _relace_map_tiles_with_objects():
+	var fuels = tilemap.get_used_cells_by_id(0)
+	for pos in fuels:
+		tilemap.set_cellv(pos, -1)
+		var instance = Fuel.instance()
+		add_child(instance)
+		var local_pos = tilemap.map_to_world(pos)
+		var global_pos = tilemap.to_global(local_pos)
+		instance.position = global_pos
+		
+	var gems = tilemap.get_used_cells_by_id(1)
+	for pos in gems:
+		tilemap.set_cellv(pos, -1)
+		var instance = Gem.instance()
+		add_child(instance)
+		var local_pos = tilemap.map_to_world(pos)
+		var global_pos = tilemap.to_global(local_pos)
+		instance.position = global_pos
 
 
 func _calculate_map_bounds():
