73de88693314053c5b27cc33f948928dcce29b4f
Justin Wong <jusw85@hotmail.com>
2021-04-25 02:59:14 +0800

    Add left right digging

--- player/player.gd
+++ player/player.gd
@@ -1,6 +1,12 @@
 extends KinematicBody2D
 
 
+class TileInfo:
+	var grid_pos: Vector2
+	var tile_id: int
+	var autotile_id: Vector2
+
+
 enum STATES {ALIVE, DEAD}
 var state = STATES.ALIVE
 
@@ -24,6 +30,8 @@ onready var tilemap: TileMap = $"../Level/TileMap"
 const DirectionalInput := preload("res://player/directional_input.gd")
 onready var dir_input: DirectionalInput = $DirectionalInput
 onready var ground_cast: RayCast2D = $GroundCast
+onready var r_cast: RayCast2D = $RightCast
+onready var l_cast: RayCast2D = $LeftCast
 
 
 func _ready() -> void:
@@ -44,20 +52,8 @@ func _unhandled_input(event: InputEvent) -> void:
 	var down_just_pressed := event.is_action_pressed("ui_down")
 	if down_just_pressed and ground_cast.is_colliding():
 		var pos := ground_cast.get_collision_point()
-		pos = tilemap.to_local(pos)
-		var grid_pos := tilemap.world_to_map(pos)
-		var tile_id := tilemap.get_cellv(grid_pos)
-		if tile_id == 4:
-			var auto_id := tilemap.get_cell_autotile_coord(grid_pos.x, grid_pos.y)
-			var rock_id := int(auto_id.x)
-			tilemap.set_cellv(grid_pos, -1)
-			match rock_id:
-				1:
-					change_fuel(-soil_fuel_needed)
-				7:
-					change_fuel(-rock_fuel_needed)
-				_:
-					change_fuel(-soil_fuel_needed)
+		pos.y += 1
+		_try_eat_rock(pos)
 
 
 func change_gem(val: int):
@@ -75,9 +71,50 @@ func change_fuel(val: int):
 	
 
 func _physics_process(_delta) -> void:
+	if state == STATES.DEAD:
+		return
+	var is_grounded = ground_cast.is_colliding()
 	var velocity = Vector2()
 	var dir = dir_input.get_input()
 	velocity.x = dir.x * walk_speed
 	velocity.y += gravity
 	velocity.y = clamp(velocity.y, 0, INF)
 	move_and_slide(velocity)
+
+	if is_grounded and r_cast.is_colliding() and dir.x > 0:
+		var pos := r_cast.get_collision_point()
+		pos.x += 1
+		_try_eat_rock(pos)
+
+	if is_grounded and l_cast.is_colliding() and dir.x < 0:
+		var pos := l_cast.get_collision_point()
+		pos.x -= 1
+		_try_eat_rock(pos)
+
+
+func _global_pos_to_tileinfo(pos: Vector2) -> TileInfo:
+	var tile_info = TileInfo.new()
+	
+	pos = tilemap.to_local(pos)
+	var grid_pos := tilemap.world_to_map(pos)
+	var tile_id := tilemap.get_cellv(grid_pos)
+	var autotile_id := tilemap.get_cell_autotile_coord(grid_pos.x, grid_pos.y)
+	
+	tile_info.grid_pos = grid_pos
+	tile_info.tile_id = tile_id
+	tile_info.autotile_id = autotile_id
+	return tile_info
+
+
+func _try_eat_rock(pos: Vector2) -> void:
+	var tileinfo := _global_pos_to_tileinfo(pos)
+	if tileinfo.tile_id == 4:
+		tilemap.set_cellv(tileinfo.grid_pos, -1)
+		var rock_id := int(tileinfo.autotile_id.x)
+		match rock_id:
+			1:
+				change_fuel(-soil_fuel_needed)
+			7:
+				change_fuel(-rock_fuel_needed)
+			_:
+				change_fuel(-soil_fuel_needed)
