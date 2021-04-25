7b72afee4db8a9779a566645f38332cb0e4f638a
Justin Wong <jusw85@hotmail.com>
2021-04-25 12:58:58 +0800

    Add gem and fuel animation

--- collectibles/fuel.gd
+++ collectibles/fuel.gd
@@ -3,6 +3,13 @@ extends Node2D
 
 export var fuel_pickup_value := 1
 
+onready var sprite: AnimatedSprite = $Area2D/AnimatedSprite
+
+func _ready():
+	var num_frames = sprite.frames.get_frame_count("flash")
+	var frame = randi() % num_frames
+	sprite.frame = randi() % num_frames
+
 
 func _on_Area2D_body_entered(body):
 	body.change_fuel(fuel_pickup_value)
--- collectibles/gem.gd
+++ collectibles/gem.gd
@@ -2,8 +2,26 @@ extends Node2D
 
 
 export var gem_pickup_value := 1
+export var flash_time := Vector2(2.0, 5.0)
+
+onready var sprite: AnimatedSprite = $Area2D/AnimatedSprite
+onready var timer: Timer = $Area2D/Timer
+
+
+func _ready():
+	timer.wait_time = rand_range(flash_time.x, flash_time.y)
+	timer.start()
 
 
 func _on_Area2D_body_entered(body):
 	body.change_gem(gem_pickup_value)
 	queue_free()
+
+
+func _on_Timer_timeout():
+	sprite.play("flash")
+
+
+func _on_AnimatedSprite_animation_finished():
+	if sprite.animation == "flash":
+		sprite.animation = "idle"
